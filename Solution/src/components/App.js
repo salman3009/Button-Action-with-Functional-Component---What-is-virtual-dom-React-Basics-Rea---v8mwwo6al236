32432eab8fb7a976bf723e46be5c19c35180f315
salman3009 <salman444g@gmail.com>
2022-10-28 19:43:53 +0530

    code

---
+++ Solution/src/components/App.js
@@ -0,0 +1,19 @@
+import React, {Component, useState} from "react";
+import "./../styles/App.css";
+
+function App() 
+{
+  const [displayMessage, setDisplayMessage] = useState(false);
+
+  return (
+	<div id="main">
+		{
+    		(displayMessage === false)?(null):(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+    	}
+		<button id="click" onClick={()=>setDisplayMessage(true)}>Click me</button>
+    </div>
+  );
+}
+
+
+export default App;
