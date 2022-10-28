1b8b1f2ee08f3bbad4bd4e6d156bbe4c095b96c7
salman3009 <salman444g@gmail.com>
2022-10-28 19:49:52 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
-function App() {
-
-  const [getFlag,setFlag] = useState(false);
+function App() 
+{
+  const [displayMessage, setDisplayMessage] = useState(false);
 
   return (
-    <div id="main">
-      // Do not alter the main div
-      {getFlag && <p id="para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
-      <button id="click" onClick={()=>setFlag(true)}>click</button>
+	<div id="main">
+		{
+    		(displayMessage === false)?(null):(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+    	}
+		<button id="click" onClick={()=>setDisplayMessage(true)}>Click me</button>
     </div>
   );
 }
 
 
 export default App;
+
